d476fda4b4677b9bd0ece354823987cdbeb100f1
Brian.quach <briquach@gmail.com>
2018-12-21 11:58:08 -0500

    Object pool

--- Super Zombie Runner/Assets/Scripts/GameObjectUtil.cs
+++ Super Zombie Runner/Assets/Scripts/GameObjectUtil.cs
@@ -1,14 +1,25 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
 
 public class GameObjectUtil {
-    
+
+    private static Dictionary<RecycleGameObject, ObjectPool> pools = new Dictionary<RecycleGameObject, ObjectPool>;
+
     public static GameObject Instantiate(GameObject prefab, Vector3 pos)
     {
         GameObject instance = null;
 
-        instance = GameObject.Instantiate(prefab);
-        instance.transform.position = pos;
+        var recycledScript = prefab.GetComponent<RecycleGameObject>();
+        if(recycledScript != null)
+        {
+            var pool = GetObjectPool(recycledScript);
+            instance = pool.NextObject(pos).gameObject;
+        } else
+        {
+            instance = GameObject.Instantiate(prefab);
+            instance.transform.position = pos;
+        }
 
         return instance;
     }
@@ -24,5 +35,23 @@ public class GameObjectUtil {
         {
             GameObject.Destroy(gameObject);
         }
+    }
+
+    private static ObjectPool GetObjectPool(RecycleGameObject reference)
+    {
+        ObjectPool pool = null;
+
+        if (pools.ContainsKey(reference))
+        {
+            pool = pools[reference];
+        } else
+        {
+            var poolContainer = new GameObject(reference.gameObject.name + "OjectPool");
+            pool = poolContainer.AddComponent<ObjectPool>();
+
+            pool.prefab = reference;
+            pools.Add(reference, pool);
+        }
+        return pool;
     }
 }
